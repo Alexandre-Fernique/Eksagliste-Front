bbf10f395843170cfc849ef22d5d242d5819ed4d
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-03-17 19:59:22 +0100

    Init project Create login component Create home component create forgot password component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import { LoginComponent } from './Page/login/login.component';
+import {MatInputModule} from "@angular/material/input";
+import {RouterModule, Routes} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
+import { ForgotPasswordComponent } from './Page/forgot-password/forgot-password.component';
+import { HomeComponent } from './Page/home/home.component';
+import {MatIconModule} from "@angular/material/icon";
 
+
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path:"forgotPassword",component:ForgotPasswordComponent},
+  {path: '**', component: LoginComponent}
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    RouterModule.forRoot(routes),
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
