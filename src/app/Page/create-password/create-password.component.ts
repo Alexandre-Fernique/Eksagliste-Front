9553c6bb23a3b26778485297291a0f4c05d06850
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-03-19 11:25:52 +0100

    Add create password page

---
+++ src/app/Page/create-password/create-password.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+import {UserService} from "../../Services/User/user.service";
+import { Router} from "@angular/router";
+
+@Component({
+  selector: 'app-create-password',
+  templateUrl: './create-password.component.html',
+  styleUrls: ['./create-password.component.css']
+})
+export class CreatePasswordComponent implements OnInit {
+  hide = true;
+  hide2 = true
+  erreur=false;
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => {
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('confirmPassword')?.value
+    return pass === confirmPass ? null : { notSame: true }
+
+  }
+  form: FormGroup = new FormGroup({
+       password: new FormControl("",[Validators.required,Validators.minLength(8),Validators.maxLength(40)]),
+       confirmPassword: new FormControl("",[Validators.required,Validators.maxLength(40),]),
+
+  },{validators:this.checkPasswords});
+  //TODO faire la validation identique
+
+  constructor(public user:UserService,public route:Router) { }
+
+  ngOnInit(): void {
+  }
+  validate(){
+    this.user.setPassword(this.route.url.split("/")[-1],this.form.value).subscribe({
+      error:(e)=>{
+
+      },
+      complete:()=>{}
+    })
+  }
+
+}
