8b53b3861a1f00362d02ae9616693be835b59293
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-03-21 13:26:41 +0100

    Push class liste

---
+++ src/app/Class/liste.ts
@@ -0,0 +1,13 @@
+export class Liste {
+  id:number
+  title:string
+  image:string
+  vote:number
+  constructor(id:number,title:string,image:string,vote:number) {
+    this.id = id
+    this.title=title
+    this.image=image
+    this.vote=vote
+  }
+
+}
