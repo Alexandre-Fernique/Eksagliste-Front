8a9cbba675f2c6bc585403654afe45b71b7e61fd
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-03-22 19:14:11 +0100

    Add signin component

--- src/app/Page/signin/signin.component.ts
+++ src/app/Page/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+import {UserService} from "../../Services/User/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  hidePassword = true;
+  hideConfirmPassword = true
+  erreur=false;
+  annees=[1,2,3,4,5]
+  formations=["PEIP","IG","MI","MSI","EGC","STE","SE","MEA","MAT","DO","GBA"]
 
-  constructor() { }
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => {
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('confirmPassword')?.value
+    return pass === confirmPass ? null : { notSame: true }
+  }
+  form: FormGroup = new FormGroup({
+    email: new FormControl("",[Validators.required,Validators.maxLength(40)]),
+    password: new FormControl("",[Validators.required,Validators.minLength(8),Validators.maxLength(40)]),
+    confirmPassword: new FormControl("",[Validators.required,Validators.maxLength(40),]),
+    formation: new FormControl("",[Validators.required]),
+    annee: new FormControl("",[Validators.required]),
+
+  },{validators:this.checkPasswords});
+  //TODO faire la validation identique
+
+  constructor(public user:UserService,public router:Router) { }
+  changeform(){
+    if (this.form.get("formation")!.value == "PEIP"){
+      this.annees=[1,2]
+    }
+    else if (this.form.get("formation")!.value == ""){
+      this.annees=[1,2,3,4,5]
+    }else{
+      this.annees=[3,4,5]
+    }
+
+  }
 
   ngOnInit(): void {
+  }
+  validate(){
+    let data = this.form.value
+    data.email = data.email + "@etu.umontpellier.fr"
+    this.user.create(data).subscribe({
+      error:(e)=>{
+        console.log(e)
+        this.erreur = true
+
+      },
+      complete:()=>{
+        console.log("ok")
+        this.router.navigate(['/']);
+
+      }
+    })
   }
 
 }
